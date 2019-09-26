b7ba7c39991f159f83927c75dbce10a3b7572ca2
Akif Kartal <51234406+akifkartal03@users.noreply.github.com>
2019-09-26 22:20:06 +0300

    main.cpp

---
+++ Project1/main.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<fstream>
+#include "source.h"
+using namespace std;
+
+int main(){
+    container contain;
+    contain=cont();
+    makefile(contain);
+    return 0;
+
+}
