f0e1fbe96f7c58d4fa17a8a8d76742b966cfb57c
Akif Kartal <51234406+akifkartal03@users.noreply.github.com>
2020-01-11 17:54:20 +0300

    Update (4)max_element_recursive.cpp
    Some changes were made.

--- Recursive_Functions/(4)max_element_recursive.cpp
+++ Recursive_Functions/(4)max_element_recursive.cpp
@@ -2,31 +2,27 @@
 #include<vector>
 using namespace std;
 template<class T>
-T maxElement(T arr[],int size);
+T maxElement(T arr[],int size,T& max);
 int main(){
 
-    int numbers[5]={5,17,-3,7,11};
-    int maxN;
-    maxN=maxElement(numbers,5);
+    int numbers[5]={5,17,-3,7,21};
+    int maxN=numbers[0];
+    maxN=maxElement(numbers,5,maxN);
     cout << "Your max element is: " << maxN << endl;
 
 }
 template<class T>
-T maxElement(T arr[],int size){
-    static T max=arr[0];
+T maxElement(T arr[],int size,T& max){
     if (size==0)//base case
     {
         return max;
     }
     else
     {
-        if (arr[0] > max)
+        if (arr[size-1] > max)
         {
-            max=arr[0];
+            max=arr[size-1];
         }
-        maxElement(&arr[1],size-1);
-    }
-    return max;
-    
-    
+        return maxElement(arr,--size,max);
+    }  
 }
